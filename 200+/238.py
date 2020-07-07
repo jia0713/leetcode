a9caead81156cdad892bedca82cba5dd5650b5f5
J.Lu <pkuljn@gmail.com>
2020-07-07 15:19:38 +0800

    Update 2020-07-07

---
+++ 200+/238.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    # 此解法未实现constant space
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        left = [1] * len(nums)
+        right = [1] * len(nums)
+        for i in range(1, len(nums)):
+            left[i] = left[i-1] * nums[i-1]
+            right[len(nums)-i-1] = right[len(nums)-i] * nums[len(nums)-i] 
+        for i in range(len(nums)):
+            left[i] = left[i] * right[i]
+        return left
