9500e6bf1a8c25ee793f3a77c6d7e2a248e56ad0
J.Lu <pkuljn@gmail.com>
2020-07-08 16:57:37 +0800

    Update 2020-07-08

---
+++ 001-100/39-Combination Sum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        candidates.sort()
+        self.res = []
+        self.backtracking(0, candidates, target, [])
+        return self.res
+        
+    def backtracking(self, index, candidates, target, path):
+        if index >= len(candidates) or candidates[index] > target:
+            return
+        if candidates[index] == target:
+            self.res.append(path + [candidates[index]])
+        self.backtracking(index, candidates, target-candidates[index], path+[candidates[index]])
+        self.backtracking(index+1, candidates, target, path)
