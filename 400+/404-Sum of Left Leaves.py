b3dcee2af364c4e68f4407acc501d6fe722f2b43
Jianing L <pkuljn@gmail.com>
2020-09-05 00:25:37 +0800

    Update 2020-09-05

---
+++ 400+/404-Sum of Left Leaves.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.res = 0
+        if not root:
+            return self.res
+        self.calLeft(root.left, "left")
+        self.calLeft(root.right, "right")
+        return self.res
+        
+    def calLeft(self, root, label):
+        if not root:
+            return
+        if not root.left and not root.right:
+            if label == "left":
+                self.res += root.val
+        self.calLeft(root.left, "left")
+        self.calLeft(root.right, "right")
