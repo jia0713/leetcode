a32be811021f02e596bb171c0f35f82ed300bd8f
J.Lu <pkuljn@gmail.com>
2021-05-15 10:25:16 +0800

    Update 2021-05-15

--- 001-100/013-Roman to Integer.py
+++ 001-100/013-Roman to Integer.py
@@ -23,3 +23,26 @@ class Solution(object):
         if pointer == len(s) - 1:
             integer += roman_dict[s[pointer]]
         return integer
+
+
+class Solution_2(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman = {"I": 1, "V": 50, "X": 50, "L": 50, "C": 100, "D": 500, "M": 1000}
+        res = 0
+        for index, lett in enumerate(s):
+            if index < len(s) - 1 and s[index : index + 2] in [
+                "IV",
+                "IX",
+                "XL",
+                "XC",
+                "CD",
+                "CM",
+            ]:
+                res -= roman[lett]
+            else:
+                res += roman[lett]
+        return res
